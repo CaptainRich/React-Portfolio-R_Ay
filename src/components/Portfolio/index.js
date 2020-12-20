620fff7b0ee876400f31496f87896db9f1d3e738
RichA <rich_ay@sbcglobal.net>
2020-12-20 13:38:56 -0600

    First pass at the 'project/portfolio' page.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,33 +2,74 @@
 import React from 'react'
 import projects from "./projects.json";
 
+function ProjectCard( props ) {
+
+  return (
+    <div className="card">
+
+      <div className="img-container">
+         <img alt={props.name} src={props.image} />
+      </div>
+
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Description:</strong> {props.description}
+          </li>
+          <li>
+            <strong>GitHub Repository:</strong> {props.gitrepo}
+            <li>
+            <strong>Deployment Link:</strong> {props.deployment}
+          </li>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  )
+
+}
+
 function Portfolio() {
 
     console.log( projects) ;
 
-    return (
-        <div className="card">
-          {/* <div className="img-container">
-            <img alt={props.name} src={props.image} />
-          </div>
-          <div className="content">
-            <ul>
-              <li>
-                <strong>Name:</strong> {props.name}
-              </li>
-              <li>
-                <strong>Occupation:</strong> {props.occupation}
-              </li>
-              <li>
-                <strong>Location:</strong> {props.location}
-              </li>
-            </ul>
-          </div>
-          <span onClick={() => props.removeFriend(props.id)} className="remove">
-            𝘅
-          </span> */}
-        </div>
+    // Declare a state variable to monitor which project is being viewed.
+    const [projectList] = useState(projects);
+
+    const renderProject = (project) => {
+
+      return (
+          // Setup the property data for the project card .
+          <ProjectCard id={project.id}
+                       name={project.name}
+                       image={project.image}
+                       description={project.description}
+                       gitrepo={project.gitrepo}
+                       deployment={project.deployment}
+                       key={project.id}
+          />
       );
+    };
+
+
+    return (
+
+      <Wrapper>
+        <h1 className="my-5" >
+          Project Portfolio
+        </h1>
+
+        {/* Map over the projects and display them. */}
+        {projectList.map( (project) => renderProject( project ))}
+      </Wrapper>
+
+    )
+
+
   
 }
 
