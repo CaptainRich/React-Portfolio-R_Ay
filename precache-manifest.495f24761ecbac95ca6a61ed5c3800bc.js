9dc10dc9699736420d008b82cc133cece1f2980e
RichA <rich_ay@sbcglobal.net>
2020-12-20 22:38:35 -0600

    Updates

---
+++ precache-manifest.495f24761ecbac95ca6a61ed5c3800bc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc8256fcaecb163e03a6eddaa2207cc",
+    "url": "/React-Portfolio-R_Ay/index.html"
+  },
+  {
+    "revision": "080be28c9bbb2faf56bc",
+    "url": "/React-Portfolio-R_Ay/static/css/main.37c25ba4.chunk.css"
+  },
+  {
+    "revision": "67b222d9005b12227551",
+    "url": "/React-Portfolio-R_Ay/static/js/2.9492d9b5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio-R_Ay/static/js/2.9492d9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080be28c9bbb2faf56bc",
+    "url": "/React-Portfolio-R_Ay/static/js/main.bd6d17d7.chunk.js"
+  },
+  {
+    "revision": "19ed70207d2d3a2e80f4",
+    "url": "/React-Portfolio-R_Ay/static/js/runtime-main.81b28b2b.js"
+  },
+  {
+    "revision": "34f1137306e39fc10abb7a67eff5e5c5",
+    "url": "/React-Portfolio-R_Ay/static/media/auto-tracker1.34f11373.jpg"
+  },
+  {
+    "revision": "4ff35d9af55bdc076d786caf9bdd619f",
+    "url": "/React-Portfolio-R_Ay/static/media/budget-tracker.4ff35d9a.jpg"
+  },
+  {
+    "revision": "bd80877aa61ff8db18449c4d68fded2e",
+    "url": "/React-Portfolio-R_Ay/static/media/employee-tracker.bd80877a.jpg"
+  },
+  {
+    "revision": "9c12a53b4b81b8b0ef25e33521df7b5b",
+    "url": "/React-Portfolio-R_Ay/static/media/mag-glass.9c12a53b.svg"
+  },
+  {
+    "revision": "856fcd192db8d4f5f228767f4b3b1bce",
+    "url": "/React-Portfolio-R_Ay/static/media/notes-taker.856fcd19.jpg"
+  },
+  {
+    "revision": "631326cea3e1808166af28c0137515d5",
+    "url": "/React-Portfolio-R_Ay/static/media/portfolio1.631326ce.jpg"
+  },
+  {
+    "revision": "6ca34edfe7deb0af0fb92d1462cef148",
+    "url": "/React-Portfolio-R_Ay/static/media/portfolio2.6ca34edf.jpg"
+  },
+  {
+    "revision": "cea2139b7ec98c37d4c493c2cf8c9ef0",
+    "url": "/React-Portfolio-R_Ay/static/media/portfolio3.cea2139b.jpg"
+  },
+  {
+    "revision": "968ab5f5b14b2880aea7e57e05dfb912",
+    "url": "/React-Portfolio-R_Ay/static/media/portfolio4.968ab5f5.jpg"
+  },
+  {
+    "revision": "f2dbac4d2c94c9055e090e9c1a3304be",
+    "url": "/React-Portfolio-R_Ay/static/media/rich.f2dbac4d.jpg"
+  },
+  {
+    "revision": "744f9042fe822932256a6947968e354b",
+    "url": "/React-Portfolio-R_Ay/static/media/stockman.744f9042.jpg"
+  },
+  {
+    "revision": "56bf037c7207416777ad41a73b6d2e3a",
+    "url": "/React-Portfolio-R_Ay/static/media/twitter.56bf037c.png"
+  },
+  {
+    "revision": "f7f6faf1b9beea607ed94da602f095b5",
+    "url": "/React-Portfolio-R_Ay/static/media/weather.f7f6faf1.jpg"
+  },
+  {
+    "revision": "3c8003536002f4cd5285d6ea6acece45",
+    "url": "/React-Portfolio-R_Ay/static/media/workday.3c800353.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio-r_ay/precache-manifest.e140cf923d5263dd5846c90a780bb44e.js"
+  "/React-Portfolio-R_Ay/precache-manifest.495f24761ecbac95ca6a61ed5c3800bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio-r_ay/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio-R_Ay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
